9d496583c9af44f0e4b96645f4180893464cf434
Hojung Kim <hojung1996@gmail.com>
2023-09-01 13:38:14 -0700

    Route to deck

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.get('/demo', (req, res) => {
   res.redirect('https://www.loom.com/share/e2ff37da4f2d421386044965dd9fbeb1');
 });
 
+app.get('/deck', (req, res) => {
+  // Redirect to a different site
+  res.redirect("https://www.figma.com/proto/hYD8ozCxLPo7NKc7626szr/Playtest-Deck?page-id=0%3A1&type=design&node-id=29-2&viewport=841%2C-684%2C0.15&t=Hx1Hu7HEtgiAAwQ0-1&scaling=contain&mode=design");
+});
+
+
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
